e15d4b966f7667e5717668a808f63b50d0f6c979
cindywgg <cindywan@andrew.cmu.edu>
2025-02-25 09:48:17 -0500

    Changed name query response

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -8,7 +8,7 @@ export default function QueryProcessor(query: string): string {
   }
 
   if (query.toLowerCase().includes("name")) {
-    return "Cindy";
+    return "cindy";
   }
 
   if (query.toLowerCase().includes("andrew id")) {
